8602b9950d56ceac37da2ba918568cdb5e9153a4
joshuaHD <joshua@tdu.cc>
2018-08-02 16:56:30 +0700

    added printAllTests function

--- testprints.js
+++ testprints.js
@@ -72,22 +72,28 @@ exports.testPrintBarcodes = function () {
         UCC_Ean_128: ["0101234567890128TEC-IT"],
     }
     // Code 128
-    
+
     lines.push(Printer.ESCPOS_CMD.BOLD)
     lines.push("invalid barcode contents can lead to wrong or no print\n\n")
     lines.push(Printer.ESCPOS_CMD.NORMAL)
-    
+
     lines.push("When printing barcodes make sure to have a leading \\n \n\n")
     for (var code in tests) {
         if (!tests.hasOwnProperty(code)) {
             //The current property is not a direct property of p
             continue;
         }
-        for (let i=0;i<tests[code].length;i++){  
-            lines.push("Trying code " + code + " = " + tests[code][i] + "\n")          
+        for (let i = 0; i < tests[code].length; i++) {
+            lines.push("Trying code " + code + " = " + tests[code][i] + "\n")
             lines.push(Printer.MAKE_BARCODE[code](tests[code][i]))
         }
     }
 
     this.printFromArray(lines)
+}
+
+exports.printAllTests = function () {
+    this.testPrintText()
+    this.testPrintQrCodes()
+    this.testPrintBarcodes()
 }
